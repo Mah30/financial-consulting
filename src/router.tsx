22cd800855adc0d822a70b5e0577fe6dcb5d56b7
Mariah <alinemariah@hotmail.com>
2026-07-02 07:55:00 +0200

    feat: add a router.tsx file

---
+++ src/router.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter(
+  [{
+    
+    children: [
+      {
+        path: '/',
+        element: <h1> Simulation Form</h1>,
+      },
+      {
+        path: '/result',
+        element: <h1> Simulation Result</h1>,
+      },
+      {
+        path: '/historico',
+        element: <h1>Histórico de Simulações</h1>,
+      },
+    ],
+},]
+)
