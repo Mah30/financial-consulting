24a1328e8f6302da22e0a39931f0115ab2a563c1
Mariah <alinemariah@hotmail.com>
2026-07-22 18:11:52 +0200

    update. add RootLayout component

--- src/router.tsx
+++ src/router.tsx
@@ -1,24 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
+import { RootLayout } from "./components/layout/RootLayout";
 
 export const router = createBrowserRouter(
   [{
     
+    element: < RootLayout />,
     children: [
       {
         path: '/',
         element: 
         
-          <h1> Simulation Form </h1>   
+          <h1>Simulation Form</h1>   
           
         ,
       },
       {
         path: '/result',
-        element: <h1> Simulation Result</h1>,
+        element: <h1>Simulation Result</h1>,
       },
       {
         path: '/historico',
-        element: <h1>Histórico de Simulações</h1>,
+        element: <h1>Simulation History</h1>,
       },
     ],
 },]
