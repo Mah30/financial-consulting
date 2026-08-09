24cb0f2156cf0fd5d7342c65f8748ac00fd3f198
Mariah <alinemariah@hotmail.com>
2026-08-09 17:05:49 +0200

    update simulationResultsPage with AIInsightCardProps and change name from Simulations to Simulation

--- src/pages/SimulationResultsPage.tsx
+++ src/pages/SimulationResultsPage.tsx
@@ -9,7 +9,7 @@ import {
 } from 'lucide-react'
 import { useParams } from 'react-router-dom'
 
-
+import { AIInsightsCard } from '../components/features/SimulationResults/AIInsightCardProps'
 import { Card } from '../components/features/SimulationResults/Card'
 import { PageHero } from '../components/shared/PageHero'
 import { useSimulationStorage } from '../components/hooks/useSimulationStorage'
@@ -55,7 +55,7 @@ export function SimulationResultsPage() {
         />
       </div>
       <div className="grid gap-6 lg:grid-cols-3">
-        
+        <AIInsightsCard simulationId={data.id} />
         <div className="order-1 flex flex-col gap-6 lg:order-2">
           <Card
             icon={Wallet}
