a4eef251a8eda7cf8f5e6a709da37f76ad634f80
Mariah <alinemariah@hotmail.com>
2026-08-16 22:44:51 +0200

    components update

--- src/components/features/SimulationResults/AIInsightCardProps.tsx
+++ src/components/features/SimulationResults/AIInsightCardProps.tsx
@@ -1,9 +1,6 @@
 import 'react-loading-skeleton/dist/skeleton.css'
-
-
-
+import Skeleton from 'react-loading-skeleton'
 import { useInsight } from '../../hooks/useInsight'
-
 import { Content } from '../Insights/Content'
 import { Error } from '../Insights/Error'
 
@@ -26,7 +23,14 @@ export function AIInsightsCard({ simulationId }: AIInsightCardProps) {
 
       {isLoading && (
         <div className="flex">
-       
+          <Skeleton
+            count={10.5}
+            baseColor="var(--color-skeleton-base)"
+            highlightColor="var(--color-skeleton-highlight)"
+            className="mb-3 flex rounded-lg"
+            containerClassName="flex-1"
+            inline
+          />
         </div>
       )}
       {!isLoading && error && (
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App'
-import { ThemeProvider } from './components/context/ThemeProvider'
+import { ThemeProvider } from './context/theme/ThemeProvider';
+import './index.css'
 
 const root = document.getElementById('root')
 
